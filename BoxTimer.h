496de6691b4d0a8c3211ba559211e7622e1b5bf9
scubpanks <scubpanks@users.noreply.github.com>
2020-06-18 22:11:23 +0200

    Add simple timer

---
+++ BoxTimer.h
@@ -0,0 +1,17 @@
+#ifndef BoxTimer_h
+#define BoxTimer_h
+#include <EnhancedThread.h>
+
+#include "BaseHeader.h"
+
+class BoxTimer {
+    public:
+        void setTimer(unsigned long milliseconds);
+        void stopTimer();
+        bool isRunning();
+    private:
+        bool _isRunning;
+        unsigned long _endMillis;
+};
+
+#endif
