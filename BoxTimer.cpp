a447ad5ef438f44f93641ab525b9cbe543439e5c
scubpanks <scubpanks@users.noreply.github.com>
2020-06-18 21:16:23 +0200

    Add a very simple timer

---
+++ BoxTimer.cpp
@@ -0,0 +1,24 @@
+#include "BoxTimer.h"
+
+void BoxTimer::setTimer(unsigned long milliseconds) { 
+    _endMillis = millis() + milliseconds;
+    _isRunning = true;
+}
+
+bool BoxTimer::isRunning() { 
+    if (! _isRunning) {
+        return false;
+    }
+
+    unsigned long currentMillis = millis();
+    if (_endMillis <= currentMillis) {
+        _isRunning = false;
+        return false;
+    }
+
+    return true;
+}
+
+void BoxTimer::stopTimer() {
+   _isRunning = false; 
+}
