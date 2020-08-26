140f6a6c8eb3591e799d5c2f1417cf05e814530b
scilor <SciLor@users.noreply.github.com>
2020-08-26 18:26:50 +0200

    Start Tonies class with protobuf decoder

--- BoxTonies.cpp
+++ BoxTonies.cpp
@@ -1,13 +1,53 @@
 #include "BoxTonies.h"
 
-#define CONTENT_BASE "/CONTENT/"
+void BoxTonies::loadTonieByUid(uint8_t uid[8]) {
+    uint8_t* path;
 
-void BoxTonies::begin() {
+    asprintf(
+        (char*)path,
+        "%s%02X%02X%02X%02X/%02X%02X%02X%02X",
+        CONTENT_BASE,
+        tagUid[0], tagUid[1], tagUid[2], tagUid[3], tagUid[4], tagUid[5], tagUid[6], tagUid[7]
+    );
 
+    loadTonieByPath(path);
 }
 
-void BoxTonies::loop() {
+void BoxTonies::loadTonieByPath(uint8_t* path) {
+    Log.info("Loading Tonie from path %s...", path);
+    
+    if (tonieFile.open((const char*)path, FA_OPEN_EXISTING | FA_READ)) {
+        uint8_t buffer[512]; //TODO: buffer >512 size may scramble the stream 4096 block needed
+        read = file.read(buffer, sizeof(buffer));
+        if (read > 0) {
+            uint16_t cursor = 0;
+            uint16_t bufferLen = 0;
 
+            while (cursor < bufferLen-1) {
+                uint8_t fieldId = buffer[cursor]>>3;
+                uint8_t fieldType = buffer[cursor]&0b00000111;
+                cursor++;
+
+                uint8_t start = cursor;
+                switch (fieldId) {
+                case 0: //Variant
+                    break;
+                case 1: //Fixed64
+                    break;
+                case 2: //Length-delimited
+                    break;
+                case 5: //Fixed32
+                    break;
+                
+                default:
+                    break;
+                }
+            }
+        } else {
+            Log.error("... could not data from file.");
+        }
+        file.close();
+    } else {
+        Log.error("... could not open Tonie.");
+    }
 }
-
-//void BoxTonies::_
--- BoxTonies.h
+++ BoxTonies.h
@@ -7,10 +7,20 @@
 class BoxTonies {
     public:
         void
-            begin(),
-            loop();
+            loadTonieByUid(uint8_t uid[8]),
+            loadTonieByPath(uint8_t* path);
+
+        struct TonieHeader {
+            uint8_t hash;
+            int64_t audioLength; //TODO check type len
+            int64_t audioId; //TODO check type len
+            uint64_t* audioChapters;//TODO check type len 
+        };
+        
 
     private:
+        static const uint8_t* CONTENT_BASE = "/CONTENT/";
+        FileFs tonieFile;
 };
 
 #endif
