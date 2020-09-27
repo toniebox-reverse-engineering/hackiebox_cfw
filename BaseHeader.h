8e7bb3215bb258e2b0e607e11294cbc464a49ccd
scilor <SciLor@users.noreply.github.com>
2020-09-27 20:26:09 +0200

    Move config static to the top

--- BaseHeader.h
+++ BaseHeader.h
@@ -1,11 +1,10 @@
 #ifndef BaseHeader_h
 #define BaseHeader_h
 
+#include "ConfigStatic.h"
 #include <Logging.h>
 #include <MemoryFree.h>
 #include "ArduinoJson-v6.15.1.h"
 #include "BoxConfig.h"
-#include "ConfigStatic.h"
-
 
 #endif
